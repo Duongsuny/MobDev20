557f4bad1377868c088fb6d1e1e2f5091529aac8
Duongsuny <duongsunny03@gmail.com>
2023-09-10 22:51:49 +0700

    done week 2 homework

---
+++ app/src/main/java/com/example/week2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.week2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import android.widget.NumberPicker;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NumberPicker numberPicker = findViewById(R.id.number);
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(1000);
+    }
+}
